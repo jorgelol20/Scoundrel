d87011d2d0ddf137bfe348e7c9e2c1c3d2a9b065
jorcolalb <jorgejorgemonovar@gmail.com>
2025-03-14 12:57:50 +0100

    JDK2

---
+++ src/Scroundrel.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBFbXgeNMGq2BIk45+vu4u+Jtl4hZ+EwbYHfyLGCHOwc7UrQcJs92WhblIUFDxQ4
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBFbXgeNMGq2BIk47fvw++WXnn4eW/cxfJ3e1vqLOOVz
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+.\..\jdk\jdk-23.0.2\bin\java.exe -jar .\..\out/artifacts/Scoundrel_jar/Scoundrel.jar
+pause
+
+
